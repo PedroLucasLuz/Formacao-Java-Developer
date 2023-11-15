e6e0eb9b7bf9254af1a36bffdf072a54bbb80823
Pedro Lucas Luz <pedro.lucas.luz083@gmail.com>
2023-11-15 16:15:08 -0300

    Tipos de Variáveis

---
+++ Exemplos/Anatomia das Classes/src/AnatomiaDasClasses/edu/PedroLucas/AnatomiaDasClasses05/AnatomiaDasClasses05_2.java
@@ -0,0 +1,7 @@
+package AnatomiaDasClasses.edu.PedroLucas.AnatomiaDasClasses05;
+
+public class AnatomiaDasClasses05_2 {
+    public static void main(String[] args) {
+        System.out.println("Estou dentro de vários pacotes :)");
+    }
+}
--- Exemplos/Anatomia das Classes/src/AnatomiaDasClasses05.java
+++ Exemplos/Anatomia das Classes/src/AnatomiaDasClasses05.java
@@ -1,3 +1,5 @@
 public class AnatomiaDasClasses05 {
-    
+    public static void main(String[] args) {
+        System.out.println("Organização de pastas e PACOTES");
+    }
 }
